751d811cdf92e0e7483cfa4c006d2004e3b4cd07
Chen Simin <375021462@qq.com>
2019-05-05 18:33:49 +0800

    updata in 190505

---
+++ docs/knowledge/design/demo/singleton.js
@@ -0,0 +1,18 @@
+/**
+ * 单例模式 的 简单实现
+ *
+ */
+
+// function Singleton() {
+//   let uniqueInstance
+
+// }
+
+class Singleton {
+  constructor() {
+    let Singleton
+    if()
+  }
+}
+
+// 使用
--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 git add -A
-git commit -m "updata in 190504"
+git commit -m "updata in 190505"
 git push origin master
